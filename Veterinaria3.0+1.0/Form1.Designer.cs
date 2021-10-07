21ade2ae22f702317d0ab6d1c6fc66ecbff52a20
Your Name <you@example.com>
2021-10-06 18:19:46 -0600

    Prueba de Hugo XD

--- Veterinaria3.0+1.0/Form1.Designer.cs
+++ Veterinaria3.0+1.0/Form1.Designer.cs
@@ -30,6 +30,8 @@ namespace Veterinaria3._0_1._0
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +43,31 @@ namespace Veterinaria3._0_1._0
             this.label1.TabIndex = 0;
             this.label1.Text = "SI SE PUEDE";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hola,";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = ", claro que si!!!";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +79,8 @@ namespace Veterinaria3._0_1._0
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
